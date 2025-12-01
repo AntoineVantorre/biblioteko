ed9bbcad3c834a47a4a39e270811238e21f805bb
Elise Magnier <elise.magnier.etu@univ-lille.fr>
2025-12-01 16:49:31 +0100

    Change structure to use Vue.js

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
 from app.api.routes import auth_router
 
 app = FastAPI()
@@ -9,3 +10,5 @@ app.include_router(auth_router, prefix="/auth")
 @app.get("/", include_in_schema=False)
 async def root():
     return RedirectResponse(url="/auth")
+
+app.mount("/static", StaticFiles(directory="/app/dist"), name="static")
---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    Hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: sans-serif;
+}
+</style>

dc447f492ce43f003c94026ee0ea83b57be54ae5
Elise Magnier <elise.magnier.etu@univ-lille.fr>
2025-12-08 14:50:44 +0100

    update file organisation

--- backend/app/db/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# app/db/models/__init__.py
-from app.db.models.user import (
-    create_user,
-)
--- backend/app/db/models/user.py
+++
@@ -1,7 +0,0 @@
-from app.db.session import get_db
-from app.schemas.user import UserCreate
-
-async def create_user(user: UserCreate):
-    db = await get_db()
-    # Logique pour créer un utilisateur
-    await db.users.insert_one(user.dict())
